35636e15fe4361922e89f71f7cbb3dc2f5029c7e
Rukhshona Olimova <rukhshona87@gmail.com>
2023-09-06 01:41:48 -0500

    Module

--- Spring-07-Spring-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
+++ Spring-07-Spring-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
@@ -3,17 +3,18 @@ package com.cydeo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller//annotate the class with @Controller stereotype annotation
 
 public class HomeController {
-    @RequestMapping("/home")
-    public String home(){
-        return "home.html";
+    @RequestMapping("/home")//Use @RequestMapping annotation to associate the action with HTTP request path
+    public String home() {
+        return "home.html";//Return the HTML document name that contains the details we want the browser to display
     }
 
 
     @RequestMapping("/welcome")
-    public String welcome(){
+    public String welcome() {
         return "welcome.html";
     }
+
 }
