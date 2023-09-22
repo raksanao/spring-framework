d888e9c20da1c29f6166e489dd5caf1739e7c13b
Rukhshona Olimova <rukhshona87@gmail.com>
2023-09-21 19:04:06 -0500

    Spring Thymeleaf css file added

--- Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -14,5 +14,10 @@ public class StudentController {
         model.addAttribute("students", DataGenerator.createStudent());
         return "student/register";
     }
+    @RequestMapping("/welcome")
+public String info(){
 
+        return "student/welcome";
+
+}
 }
