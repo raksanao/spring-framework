70acf05cba74ecf84d068de59b77d19f61e40d3b
Rukhshona Olimova <rukhshona87@gmail.com>
2023-10-26 16:30:22 -0500

    new coomit

---
+++ Spring-07-Spring-MVC-Controller/src/main/java/com/cydeo/controller/ControllerApplication1.java
@@ -0,0 +1,13 @@
+package com.cydeo.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerApplication1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControllerApplication1.class, args);
+    }
+
+}
---
+++ Spring-11_ORM-Mapping-OneToOne/src/test/java/Spring11OrmMappingOneToOneApplicationTests.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring11OrmMappingOneToOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
